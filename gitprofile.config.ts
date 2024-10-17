815d37e1718d378c9d5f7a0d8295532aabb2b770
Mashud <mashud@localhost.localdomain>
2024-10-17 12:54:46 +0600

    .

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -180,7 +180,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'night',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
